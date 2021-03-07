b686cdf38f45d2d5b2d6c41fc31f9ec34010920c
MakkoXie <yimingx@usc.edu>
2021-03-07 21:29:02 +0800

    hhhh

--- ask-your-doctor/src/pages/Chatroom.vue
+++ ask-your-doctor/src/pages/Chatroom.vue
@@ -1,8 +1,6 @@
 <template>
-  <basic-vue-chat :initial-feed="feed" />
+  <basic-vue-chat :title="'Your Personal Doctor'" :initial-feed="feed" style="height: 1000px"/>
 </template>
-
-
 <script>
 import BasicVueChat from 'basic-vue-chat'
 
@@ -11,21 +9,28 @@ export default {
     "basic-vue-chat": BasicVueChat
   },
   data() {
-    const feed = [
-      {
-        id: 1,
-        author: 'Person',
-        imageUrl: 'http://path/to/image',
-        contents: 'hi there',
-        date: '16:30'
-      },
-      {
-        id: 0,
-        author: 'Me',
-        contents: 'hello',
-        date: '16:30'
-      }
-    ]
+    return {
+      feed: [
+        {
+          id: 1,
+          author: 'Dr. One',
+          // imageUrl: 'http://path/to/image',
+          contents: 'hi there',
+          date: '16:30'
+        },
+        {
+          id: 0,
+          author: 'Me',
+          contents: 'hello',
+          date: '16:30'
+        }
+      ]
+    }
   }
 }
 </script>
+<style lang="scss">
+// above the following import you can override default values of variables like $primary
+@import "../assets/scss/main.scss";
+</style>
+
