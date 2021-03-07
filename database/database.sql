9e750cd5f8174a8aad91ae330707eb288fbcaa41
Shuning Zhang <shuningz@usc.edu>
2021-03-06 22:45:18 -0800

    Update database

--- database/database.sql
+++ database/database.sql
@@ -26,10 +26,62 @@ CREATE TABLE IF NOT EXISTS `skkkzhan_athenahack2021_db`.`users` (
   `isDoctor` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'If the user is a doctor, 0 for no, 1 for yes',
   `emergencycontact1` JSON NULL DEFAULT NULL,
   `emergencycontact2` JSON NULL DEFAULT NULL,
+  `location` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`userid`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COMMENT = 'All registered users\' information';
+
+
+-- -----------------------------------------------------
+-- Table `skkkzhan_athenahack2021_db`.`blogs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `skkkzhan_athenahack2021_db`.`blogs` (
+  `idblogs` INT(11) NOT NULL,
+  `content` VARCHAR(4000) NOT NULL,
+  `userid` INT(11) NOT NULL,
+  PRIMARY KEY (`idblogs`),
+  INDEX `fk_blogs_users1_idx` (`userid` ASC),
+  CONSTRAINT `fk_blogs_users1`
+    FOREIGN KEY (`userid`)
+    REFERENCES `skkkzhan_athenahack2021_db`.`users` (`userid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `skkkzhan_athenahack2021_db`.`tags`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `skkkzhan_athenahack2021_db`.`tags` (
+  `idtags` INT(11) NOT NULL,
+  `tagname` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idtags`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `skkkzhan_athenahack2021_db`.`blog_tag`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `skkkzhan_athenahack2021_db`.`blog_tag` (
+  `blogid` INT(11) NOT NULL,
+  `tagid` INT(11) NOT NULL,
+  INDEX `fk_blog_tag_blogs1_idx` (`blogid` ASC),
+  INDEX `fk_blog_tag_tags1_idx` (`tagid` ASC),
+  CONSTRAINT `fk_blog_tag_blogs1`
+    FOREIGN KEY (`blogid`)
+    REFERENCES `skkkzhan_athenahack2021_db`.`blogs` (`idblogs`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_blog_tag_tags1`
+    FOREIGN KEY (`tagid`)
+    REFERENCES `skkkzhan_athenahack2021_db`.`tags` (`idtags`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -43,8 +95,8 @@ CREATE TABLE IF NOT EXISTS `skkkzhan_athenahack2021_db`.`chats` (
   `sent_time` DATETIME NOT NULL,
   `isImage` TINYINT(4) NOT NULL DEFAULT '0' COMMENT 'If this chat sends an image, 0 for no, 1 for yes, default no',
   PRIMARY KEY (`idchats`),
-  INDEX `fk_chats_users_idx` (`from` ASC) VISIBLE,
-  INDEX `fk_chats_users1_idx` (`to` ASC) VISIBLE,
+  INDEX `fk_chats_users_idx` (`from` ASC),
+  INDEX `fk_chats_users1_idx` (`to` ASC),
   CONSTRAINT `fk_chats_users`
     FOREIGN KEY (`from`)
     REFERENCES `skkkzhan_athenahack2021_db`.`users` (`userid`)
@@ -60,72 +112,25 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `skkkzhan_athenahack2021_db`.`blogs`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `skkkzhan_athenahack2021_db`.`blogs` (
-  `idblogs` INT(11) NOT NULL,
-  `content` VARCHAR(4000) NOT NULL,
-  `userid` INT(11) NOT NULL,
-  PRIMARY KEY (`idblogs`),
-  INDEX `fk_blogs_users1_idx` (`userid` ASC) VISIBLE,
-  CONSTRAINT `fk_blogs_users1`
-    FOREIGN KEY (`userid`)
-    REFERENCES `skkkzhan_athenahack2021_db`.`users` (`userid`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `skkkzhan_athenahack2021_db`.`tags`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `skkkzhan_athenahack2021_db`.`tags` (
-  `idtags` INT(11) NOT NULL,
-  `tagname` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idtags`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `skkkzhan_athenahack2021_db`.`likes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `skkkzhan_athenahack2021_db`.`likes` (
   `userid` INT(11) NOT NULL,
   `blogid` INT(11) NOT NULL,
-  INDEX `fk_likes_users1_idx` (`userid` ASC) VISIBLE,
-  INDEX `fk_likes_blogs1_idx` (`blogid` ASC) VISIBLE,
-  CONSTRAINT `fk_likes_users1`
-    FOREIGN KEY (`userid`)
-    REFERENCES `skkkzhan_athenahack2021_db`.`users` (`userid`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `fk_likes_users1_idx` (`userid` ASC),
+  INDEX `fk_likes_blogs1_idx` (`blogid` ASC),
   CONSTRAINT `fk_likes_blogs1`
     FOREIGN KEY (`blogid`)
     REFERENCES `skkkzhan_athenahack2021_db`.`blogs` (`idblogs`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `skkkzhan_athenahack2021_db`.`blog_tag`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `skkkzhan_athenahack2021_db`.`blog_tag` (
-  `blogid` INT(11) NOT NULL,
-  `tagid` INT(11) NOT NULL,
-  INDEX `fk_blog_tag_blogs1_idx` (`blogid` ASC) VISIBLE,
-  INDEX `fk_blog_tag_tags1_idx` (`tagid` ASC) VISIBLE,
-  CONSTRAINT `fk_blog_tag_blogs1`
-    FOREIGN KEY (`blogid`)
-    REFERENCES `skkkzhan_athenahack2021_db`.`blogs` (`idblogs`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_blog_tag_tags1`
-    FOREIGN KEY (`tagid`)
-    REFERENCES `skkkzhan_athenahack2021_db`.`tags` (`idtags`)
+  CONSTRAINT `fk_likes_users1`
+    FOREIGN KEY (`userid`)
+    REFERENCES `skkkzhan_athenahack2021_db`.`users` (`userid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
